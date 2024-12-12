28c6204df00b4579ed7ca16ad0dca5d0ede298e0
emreutkan <127414322+emreutkan@users.noreply.github.com>
2024-12-12 20:58:33 +0300

    Refactor app structure and remove redundant features.
    Removed outdated authentication-related files and services, consolidating functionality into a cleaner modular structure. Introduced database initialization and connection checks in `app.py` to enhance reliability. Established a new structure for routes and models for scalability and maintainability.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+
+import sqlalchemy
 from flask import Flask, render_template
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
@@ -43,7 +45,23 @@ def create_app():
     app.config['JWT_SECRET_KEY'] = required_env_vars['JWT_SECRET_KEY']
     JWTManager(app)
 
+
     db.init_app(app)  # Initialize the database
+
+    with app.app_context():
+        db.create_all()
+
+    # Check database connection
+    try:
+        engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+        connection = engine.connect()
+        connection.close()
+        print("Database connection successful.")
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+        # Here you might want to handle the error,
+        # such as exiting the application or using a fallback configuration.
+
     init_app(app)  # Register the blueprints using the init_app function
 
     return app
