b3a8123a632c750275e975bcd9cbe512c243ef6c
emreutkan <127414322+emreutkan@users.noreply.github.com>
2024-12-12 21:00:54 +0300

    Add auth route initialization module
    Introduced an `__init__.py` file for the auth route to register blueprints for login and registration. This prepares the groundwork for modularizing the authentication endpoints.

---
+++ routes/auth/__init__.py
@@ -0,0 +1,8 @@
+# from .login import login_bp
+# from .register import register_bp
+#
+#
+# def init_app(app):
+#     # Version 1 Blueprints
+#     app.register_blueprint(login_bp, url_prefix='/v1/auth')
+#     app.register_blueprint(register_bp, url_prefix='/v1/listing')
