67d412877526e1f30fa859eb7a548a4235d496fc
emreutkan <127414322+emreutkan@users.noreply.github.com>
2024-12-28 15:55:31 +0300

    fetch user data

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from .auth.login import login_bp
 from .auth.register import register_bp
 from .customerAddressManager import customerAddressManager_bp
-
+from .user import user_bp
 def init_app(app):
     # Create a versioned API blueprint
     api_v1 = Blueprint('api_v1', __name__, url_prefix='/v1')
@@ -11,6 +11,7 @@ def init_app(app):
     api_v1.register_blueprint(login_bp)
     api_v1.register_blueprint(register_bp)
     api_v1.register_blueprint(customerAddressManager_bp)
+    api_v1.register_blueprint(user_bp)
 
     # Register the versioned API blueprint with the main app
     app.register_blueprint(api_v1)
