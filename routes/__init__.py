4f6400077ae81e772714f2a8f0360759dfdbc4d1
Irfan Emre Utkan <127414322+emreutkan@users.noreply.github.com>
2025-01-06 18:02:19 +0300

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -5,6 +5,7 @@ from .customerAddressManager import customerAddressManager_bp
 from .restaurantManager import restaurantManager_bp
 from .user import user_bp
 from .listings import listings_bp
+from .cart import cart_bp
 from .search import search_bp
 def init_app(app):
     # Create a versioned API blueprint
@@ -17,7 +18,7 @@ def init_app(app):
     api_v1.register_blueprint(restaurantManager_bp)
     api_v1.register_blueprint(user_bp)
     api_v1.register_blueprint(listings_bp)
+    api_v1.register_blueprint(cart_bp)
     api_v1.register_blueprint(search_bp)
-
     # Register the versioned API blueprint with the main app
     app.register_blueprint(api_v1)
