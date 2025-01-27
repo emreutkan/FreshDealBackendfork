e0dc4890daf924b3cf3cd54511d9fe6d9de0bfbe
emreutkan <127414322+emreutkan@users.noreply.github.com>
2025-01-27 14:02:49 +0300

    fix email issues blocking a register process

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -2,13 +2,12 @@ from sqlalchemy.orm import relationship
 from . import db
 from sqlalchemy import Integer, String, CheckConstraint, Boolean
 
-
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(Integer, primary_key=True, autoincrement=True)
     name = db.Column(String(80), nullable=False)
-    email = db.Column(String(250), unique=True, nullable=False)
-    phone_number = db.Column(String(15), unique=True, nullable=False)
+    email = db.Column(String(250), unique=True, nullable=False)  # Must be unique
+    phone_number = db.Column(String(15), unique=True, nullable=False)  # Must be unique
     password = db.Column(String(1280), nullable=False)
     role = db.Column(String(20), nullable=False, default='customer')
     email_verified = db.Column(Boolean, nullable=False, default=False)
@@ -22,5 +21,5 @@ class User(db.Model):
     comments = relationship('RestaurantComment', back_populates='user')
     devices = relationship('UserDevice', back_populates='user', lazy=True)
 
-    reset_token = db.Column(db.String(100), unique=True, nullable=True)
+    reset_token = db.Column(db.String(100), unique=False, nullable=True)  # NULL or unique
     reset_token_expires = db.Column(db.DateTime, nullable=True)
